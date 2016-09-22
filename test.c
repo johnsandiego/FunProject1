7eb0ef2579fa48e21a70fdf389c9cc9da5ba9248
johnsandiego <johnpatrick415@gmail.com>
2016-09-22 19:49:43 -0400

    added opcode 5 &6

--- test.c
+++ test.c
@@ -1,61 +1,46 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-void OP4(char *IR);
-void PrintIR(char *IR) ;
-int ACC;
+int ParseOp1(char *IR);
+void OP6(char *IR);
+int ParseOp1Reg(char *IR);
+int ParseOP1andOP2Imm(char *IR);
+
 char IR[6];
-char memory[100][6];
-char x;
-char *y;
-short int P0 ; //these are the pointer registers
-short int P1 ;
-short int P2 ;
-short int P3 ;
-main(){
+int operand;
+int main(){
 
-  int i;
-    printf("enter 6 character: \n");
-    //scanf("%c\n", IR[4] );
-    scanf("%c\n", &IR[0] );
-    scanf("%c", &IR[1] );
-    scanf("%c\n", &IR[2] );
-    scanf("%c", &IR[3] );
-    scanf("%c\n", &IR[4] );
-    scanf("%c", &IR[5] );
-    //ACC = IR[4]-'0' + IR[5]-'0';
-  //  printf("%c", IR[4]);
-    //printf("%c\n", IR[5]);
-    //printf("%i\n", ACC);
-    for( i = 0; i<6;i++){
-    printf("%i\n", IR[i]-'0');
+  IR[0] = '0';
+  IR[1] = '4';
+  IR[2] = '0';
+  IR[3] = '0';
+  IR[4] = '0';
+  IR[5] = '9';
+
+  //ParseOp1(IR);
+
+
+  printf("%i\n", ParseOP1andOP2Imm(IR));
+
+}
+
+int ParseOp1 (char *IR)
+    {
+
+      operand = ((IR[2]-48)*10+(IR[3]-48)*1);
+      return operand;
+
     }
-    OP4(IR);
+
+int ParseOp1Reg(char *IR){
+
+
 
 }
 
-void OP4(char *IR){
-  int PREG, VAL, ADDR ;
+int ParseOP1andOP2Imm(char *IR)
+    {
+      int ParseOP1andOP2;
+      ParseOP1andOP2 = ((IR[2]-48)*1000+(IR[3]-48)*100+(IR[4]-48)*10+(IR[5]-48)*1);
+        return ParseOP1andOP2;
 
 
-  	 PREG = 2; // PREG = pointer register
-     P2 = 2;
-     //PREG = 12 ;
-     //pointer register = operand 1
-     //memory location = pointer register
-     //acc = memory location
-
-	switch(PREG)
-		{
-      case 0: ADDR = P0;
-		  case 1: ADDR = P1;
-		  case 2: ADDR = P2;
-		  case 3: ADDR = P3;
-		}
-    VAL = (memory[ADDR][2]-48)*1000;
-    VAL += (memory[ADDR][3]-48)*100;
-    VAL += (memory[ADDR][4]-48)*10;
-    VAL += (memory[ADDR][5]-48)*1;
-    ACC = VAL;
-  printf("%i\n", ACC);
-}
+    }
